9043a0296e493d8f619fa0114eb4889b5ca22be0
Harsh Der <126702940+harshder003@users.noreply.github.com>
2023-12-13 14:33:55 +0530

    Update ProcessDashboard api to arsdemo

--- src/routes/ProcessDashboard.jsx
+++ src/routes/ProcessDashboard.jsx
@@ -11,7 +11,7 @@ const ProcessDashboard = () => {
     async function getProcessStats() {
       setIsLoading(true);
       try {
-        const response = await fetch("https://api.fjreview.work/process_stats", {
+        const response = await fetch("https://arsdemo.fjreview.work/process_stats", {
         // const response = await fetch("http://localhost:3000/process_stats", {
           method: "GET",
           headers: {
