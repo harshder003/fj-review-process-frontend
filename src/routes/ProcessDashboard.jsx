228a124d6e37649d0ba8b0709aebb0ff417597f0
Harsh Der <126702940+harshder003@users.noreply.github.com>
2024-03-06 13:00:20 +0530

    Update ProcessDashboard.jsx with ngrok header

--- src/routes/ProcessDashboard.jsx
+++ src/routes/ProcessDashboard.jsx
@@ -17,6 +17,7 @@ const ProcessDashboard = () => {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
+             'ngrok-skip-browser-warning': 'true',
           },
         });
         const data = await response.json();
