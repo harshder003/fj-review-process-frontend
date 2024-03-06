276ad459e4dfb9b6c91373a431774da8db08b908
Harsh Der <126702940+harshder003@users.noreply.github.com>
2024-03-06 21:33:29 +0530

    Update EndDate.jsx new ngrok

--- src/routes/EndDate.jsx
+++ src/routes/EndDate.jsx
@@ -20,7 +20,8 @@ const EndDate = () => {
     async function getEndDate() {
       setLoadingFields(true);
       const response = await fetch(
-        `https://arsprod.fjreview.work/get_end_date/${id}/${projectId}`,
+        // `https://arsprod.fjreview.work/get_end_date/${id}/${projectId}`,
+        `https://ef15-122-171-236-173.ngrok-free.app/get_end_date/${id}/${projectId}`,
         // `http://localhost:3000/get_end_date/${id}/${projectId}`,
         {
           method: "GET",
