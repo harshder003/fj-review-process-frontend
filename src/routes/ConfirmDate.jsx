2dc0d5c0297cf695f6da2df8e3a6e2a10b74ff50
Harsh Der <126702940+harshder003@users.noreply.github.com>
2024-03-06 21:33:44 +0530

    Update ConfirmDate.jsx new ngrok

--- src/routes/ConfirmDate.jsx
+++ src/routes/ConfirmDate.jsx
@@ -19,7 +19,7 @@ const ConfirmDate = () => {
     async function getConfirmDate() {
       setLoadingFields(true);
       const response = await fetch(
-        `https://arsprod.fjreview.work/confirming_end_date/${id}/${projectId}`,
+        // `https://arsprod.fjreview.work/confirming_end_date/${id}/${projectId}`,
         `https://ef15-122-171-236-173.ngrok-free.app/confirming_end_date/${id}/${projectId}`,
         // `http://localhost:3000/confirming_end_date/${id}/${projectId}`,
         {
